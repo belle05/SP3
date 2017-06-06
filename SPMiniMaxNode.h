366bbe122d572d302299b76c66542ce0c0774a31
belle05 <cbfe510@gmail.com>
2017-06-06 19:27:31 +0300

    updated signatures in SPMiniMaxNode.h according to new functions

--- SPMiniMaxNode.h
+++ SPMiniMaxNode.h
@@ -1,6 +1,14 @@
 #ifndef SPMINIMAXNODE_H_
 #define SPMINIMAXNODE_H_
 
-//Put all decleartions and constants here
+int calcBoardScore(SPFiarGame* src,  char player1, char player2);
+
+int singleScore(int score);
+
+int calcBoadRows(SPFiarGame* src, char player1, char player2);
+
+int calcBoadCols(SPFiarGame* src);
+
+int calcBoadDiagonals(SPFiarGame* src);
 
 #endif
