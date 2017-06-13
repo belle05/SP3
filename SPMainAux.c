9cf17d24099e63ed98eade15b459030eff32eb77
belle05 <cbfe510@gmail.com>
2017-06-13 17:21:59 +0300

    added main.c and SPMainAux.c

---
+++ SPMainAux.c
@@ -0,0 +1,2 @@
+#include "SPMainAux.h"
+
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "SPMainAux.h"
+#include "SPMiniMaxNode.h"
+#include "SPFIARGame.h"
+#include "SPMiniMax.h"
+
+int main() {
+
+	while(true){
+
+	}
+	return 0;
+}
